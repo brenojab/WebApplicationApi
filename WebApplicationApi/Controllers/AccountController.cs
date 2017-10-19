bcd2fdf46e8b27d0e90ec595b25299fb5081c77d
Breno Batista <brenojab@gmail.com>
2017-10-19 05:26:23 -0200

    Consegui fazer o post pelo ValuesController, usando atributos do ASP.Net Core.

---
+++ WebApplicationApi/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplicationApi.Controllers
+{
+  [Produces("application/json")]
+  [System.Web.Http.Route("api/Account")]
+  public class AccountController : ApiController
+  {
+    private AuthRepository _repo = null;
+
+    public AccountController()
+    {
+      _repo = new AuthRepository();
+    }
+
+    // POST api/Account/Register
+    [AllowAnonymous]
+    [System.Web.Http.Route("Register")] //todo Avaliar se é system.web.http mesmo
+    public async Task<IHttpActionResult> Register(UserModel userModel)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      IdentityResult result = await _repo.RegisterUser(userModel);
+
+      IHttpActionResult errorResult = GetErrorResult(result);
+
+      if (errorResult != null)
+      {
+        return errorResult;
+      }
+
+      return Ok();
+    }
+
+    // POST api/values
+    [Microsoft.AspNetCore.Mvc.HttpPost]
+    public void Post()
+    {
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        _repo.Dispose();
+      }
+
+      base.Dispose(disposing);
+    }
+
+    private IHttpActionResult GetErrorResult(IdentityResult result)
+    {
+      if (result == null)
+      {
+        return InternalServerError();
+      }
+
+      if (!result.Succeeded)
+      {
+        if (result.Errors != null)
+        {
+          foreach (string error in result.Errors)
+          {
+            ModelState.AddModelError("", error);
+          }
+        }
+
+        if (ModelState.IsValid)
+        {
+          // No ModelState errors are available to send, so just return an empty BadRequest.
+          return BadRequest();
+        }
+
+        return BadRequest(ModelState);
+      }
+
+      return null;
+    }
+  }
+}
